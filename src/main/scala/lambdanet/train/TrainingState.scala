8c41e31c39a1bb2ffe62f5f9870066701378c9cf
Jiayi Wei <MrVPlusOne@gmail.com>
2019-09-23 18:04:05 -0500

    Ablation: iter=2

--- src/main/scala/lambdanet/train/TrainingState.scala
+++ src/main/scala/lambdanet/train/TrainingState.scala
@@ -17,7 +17,7 @@ object TrainingState {
     TrainingState(step, dimMessage, iterationNum, optimizer)
   }
 
-  val iterationNum: Int = 1
+  val iterationNum: Int = 2
 
   def loadTrainingState(
       resultsDir: Path,
