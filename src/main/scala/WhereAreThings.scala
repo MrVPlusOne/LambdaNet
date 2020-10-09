5208c6170c6e106f6ca8eb495866e5b1e3c87bfd
Jiayi Wei <wjydzh1@gmail.com>
2020-10-09 14:34:42 -0500

    Update documentation.

---
+++ src/main/scala/WhereAreThings.scala
@@ -0,0 +1,12 @@
+import lambdanet.translation.PredicateGraph
+import lambdanet.LibDefs
+
+/**
+ * Type constraints/predicates are defined in [[PredicateGraph]].
+ * Examples showing how to generate predicate graphs from source code are
+ * inside test/scala/lambdanet/utils. Particularly under the test named
+ * "predicate graph tests".
+ * Library definitions are stored as a [[LibDefs]] class. But the current
+ * format might not be optimized for performing type checking.
+*/
+object WhereAreThings
