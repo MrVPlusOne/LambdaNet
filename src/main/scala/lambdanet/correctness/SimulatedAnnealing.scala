e3a0294fb9978ee29c9e6badfbfa144364311c4c
yhylord <noneofyourbusiness@example.com>
2020-11-13 18:56:39 -0600

    Add wrong-nodes-first neighborhood, debugging output, and fix None.get bug

--- src/main/scala/lambdanet/correctness/SimulatedAnnealing.scala
+++ src/main/scala/lambdanet/correctness/SimulatedAnnealing.scala
@@ -7,41 +7,61 @@ import lambdanet.translation.PredicateGraph.{PAny, PNode, PType}
 import scala.util.Random
 
 object SimulatedAnnealing {
+  def diff(
+    a: Assignment,
+    b: Assignment
+  ): Map[PNode, (PType, PType)] = {
+    val set =
+      for (node <- a.keySet if a(node) != b(node))
+        yield (node, (a(node), b(node)))
+    set.toMap
+  }
+
   def search(
-      g: PredicateGraph,
-      proposal: Map[PNode, TopNDistribution[PType]],
-      randomNeighbor: Assignment => Assignment,
-      correct: Correction,
-      t: Int => Double,
-      numEpochs: Int,
-      f: Objective
+    g: PredicateGraph,
+    proposal: Map[PNode, TopNDistribution[PType]],
+    randomNeighbor: Assignment => Assignment,
+    correct: Correction,
+    t: Int => Double,
+    numEpochs: Int,
+    f: Objective
   ): Assignment = {
     val mostLikely = proposal.mapValuesNow(_.topValue)
     var x = mostLikely
     var y = f(correct(x))
     var bestX = x
     var bestY = y
+    var lastBestEpoch = 0
     (1 to numEpochs).foreach { epoch =>
       val nextX = randomNeighbor(x)
-      val nextY = f(correct(nextX))
+      val correctX = correct(nextX)
+      println(epoch)
+      val nextY = f(correctX)
       val deltaY = nextY - y
       if (deltaY >= 0 || Random.nextDouble() < math.exp(deltaY / t(epoch))) {
         x = nextX
         y = nextY
       }
-      if (y < bestY) {
+      println(s"y: $y, bestY: $bestY")
+      if (y > bestY) {
         bestX = x
         bestY = y
+        lastBestEpoch = epoch
+      }
+      if (epoch - lastBestEpoch > numEpochs / 20) {
+        x = bestX
+        y = bestY
       }
     }
-    bestX
+    correct(bestX)
   }
 }
 
-case class OneDifferenceRandomNeighbor(proposal: Map[PNode, TopNDistribution[PType]]) {
+trait OneDifferenceRandomNeighborBase {
+  def proposal: Map[PNode, TopNDistribution[PType]]
+
   // TODO: Pass a vector for better performance
-  def randomNeighbor(x: Assignment): Assignment = {
-    val nodeIndex = Random.nextInt(x.size)
+  def changeType(x: Assignment, nodeIndex: Int): Assignment = {
     val vec: Seq[(PNode, PType)] = x.toVector
     val (node, oldType) = vec(nodeIndex)
     val distr = proposal(node).distr
@@ -54,9 +74,39 @@ case class OneDifferenceRandomNeighbor(proposal: Map[PNode, TopNDistribution[PTy
       .next()
     vec.updated(nodeIndex, (node, newType)).toMap
   }
+
+  def randomNeighbor(x: Assignment): Assignment = {
+    val nodeIndex = Random.nextInt(x.size)
+    changeType(x, nodeIndex)
+  }
+}
+
+case class OneDifferenceRandomNeighbor(
+  proposal: Map[PNode, TopNDistribution[PType]]
+) extends OneDifferenceRandomNeighborBase
+
+case class WrongFirstOneDifferenceRandomNeighbor(
+  override val proposal: Map[PNode, TopNDistribution[PType]],
+  checker: TypeChecker
+) extends OneDifferenceRandomNeighborBase {
+  override def randomNeighbor(x: Assignment): Assignment = {
+    val badPairs = checker.violate(x).toIndexedSeq
+    if (badPairs.nonEmpty) {
+      val pairIndex = Random.nextInt(badPairs.size)
+      val node = Iterator.continually {
+        val elementIndex = Random.nextInt(2)
+        badPairs(pairIndex).productElement(elementIndex)
+      }.dropWhile(n => n.asInstanceOf[PNode].fromLib).next()
+      val nodeIndex = x.toIndexedSeq.indexWhere { case (n, _) => n == node }
+      changeType(x, nodeIndex)
+    } else {
+      super.randomNeighbor(x)
+    }
+  }
 }
 
 trait PatchAny {
+
   /**
     * Try to mark as few nodes as `Any` to satisfy all the subtyping relations in `badPairs`.
     * Treat this as the Minimum Vertex Cover problem and use the approximation algorithm.
@@ -112,14 +162,15 @@ case class LocalSearchCorrection(
     val badNodes = badPairs.flatMap { case (a, b) => Set(a, b) }
     var (leastBadPairs, bestPrediction) = (badPairs, prediction)
     badNodes.foreach { node =>
-      proposal(node).distr.map(_._2).foreach {
-        case newType if newType != prediction(node) =>
+      proposal(node).distr.map(_._2).foreach { newType =>
+        if (newType != prediction(node)) {
           val (newBadPairs, newPrediction) =
             boundedSearch(prediction.updated(node, newType), remainingDepth - 1)
           if (newBadPairs.size < badPairs.size) {
             leastBadPairs = badPairs
             bestPrediction = newPrediction
           }
+        }
       }
     }
     (leastBadPairs, bestPrediction)
