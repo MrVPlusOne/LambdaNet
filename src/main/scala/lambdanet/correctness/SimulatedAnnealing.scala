386ebfe06b2e1bbaa9e6e88e370cb667d4e6857b
Jiayi Wei <wjydzh1@gmail.com>
2020-11-19 18:02:15 -0600

    Add some comments on correctness code.

--- src/main/scala/lambdanet/correctness/SimulatedAnnealing.scala
+++ src/main/scala/lambdanet/correctness/SimulatedAnnealing.scala
@@ -166,6 +166,7 @@ case class WrongFirstOneDifferenceRandomNeighbor(
 ) extends OneDifferenceRandomNeighborBase {
   override def randomNeighbor(x: Assignment): Assignment = {
     val badPairs = checker.violate(x).toIndexedSeq
+    // fixme: can we try assigning this branch with some probability `p < 1` ?
     if (badPairs.nonEmpty) {
       val pairIndex = Random.nextInt(badPairs.size)
       val node = Iterator
@@ -264,6 +265,8 @@ case class NegativeLogLikelihood(
     -assignment.map {
       case (node, typ) =>
         val topN = proposal(node)
+        //fixme: this might be expensive when `N` is large. Should we turn
+        // `TopNDistribution` into a map first?
         val topNProb = topN.distr.find(_._2 == typ).map(_._1)
         val probOther = 1 - topN.distr.map(_._1).take(10).sum
         math.log(topNProb.getOrElse(probOther))
