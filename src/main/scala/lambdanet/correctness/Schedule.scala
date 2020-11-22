a224e752e8567938351249827433aae8721a5b46
yhylord <noneofyourbusiness@example.com>
2020-11-21 22:38:42 -0600

    Add exponential schedule and fast schedule

--- src/main/scala/lambdanet/correctness/Schedule.scala
+++ src/main/scala/lambdanet/correctness/Schedule.scala
@@ -13,6 +13,22 @@ case class LogSchedule(t0: Double) extends Schedule {
   val description: String =
     "Logarithmic schedule (t(k) = t0 * log(2) / log(k+1))"
 
-  override def apply(epoch: Int): Double =
+  def apply(epoch: Int): Double =
     t0 * math.log(2) / math.log(epoch + 1)
+}
+
+case class ExpSchedule(t0: Double, gamma: Double) extends Schedule {
+  val description: String =
+    "Exponential decay schedule (t(k) = t0 * gamma^(k-1))"
+
+  def apply(epoch: Int): Double =
+    t0 * math.pow(gamma, epoch - 1)
+}
+
+case class FastSchedule(t0: Double) extends Schedule {
+  val description: String =
+    "Fast schedule (t(k) = t0 / k)"
+
+  def apply(epoch: Int): Double =
+    t0 / epoch
 }
