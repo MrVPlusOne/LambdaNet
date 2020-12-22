672f20740bf8001e43a03bdab1a798e35711f3b1
yhylord <noneofyourbusiness@example.com>
2020-12-22 03:39:02 -0600

    Cache subtype relations

--- src/main/scala/lambdanet/correctness/TypeChecker.scala
+++ src/main/scala/lambdanet/correctness/TypeChecker.scala
@@ -2,22 +2,20 @@ package lambdanet.correctness
 
 import lambdanet.LibDefs
 import lambdanet.translation.PredicateGraph
-import lambdanet.translation.PredicateGraph.{
-  BinaryRel,
-  BinaryRelCat,
-  PNode,
-  PType
-}
+import lambdanet.translation.PredicateGraph.{BinaryRel, BinaryRelCat, PNode, PType}
 
 case class TypeChecker(
-    graph: PredicateGraph,
-    libDefs: LibDefs,
-    additionalSubrel: Set[(PType, PType)] = Set.empty
+  graph: PredicateGraph,
+  libDefs: LibDefs,
+  binaryRels: Set[BinaryRel],
+  subtypesToCheck: Set[(PNode, PNode)],
+  defaultContext: PTypeContext,
+  additionalSubrel: Set[(PType, PType)]
 ) {
   def violate(
       assignment: Map[PNode, PType],
   ): Set[(PNode, PNode)] =
-    violate(PTypeContext(graph, libDefs, additionalSubrel), assignment)
+    violate(defaultContext, assignment)
 
   def violate(
       context: PTypeContext,
@@ -29,6 +27,18 @@ case class TypeChecker(
         s"Nodes only in assignment: ${assignment.keySet &~ graph.nodes}\n" +
         s"Nodes only in graph: ${graph.nodes &~ assignment.keySet}"
     )
+    subtypesToCheck.filterNot {
+      case (child, parent) => context.isSubtype(child, parent, assignment)
+    }
+  }
+}
+
+object TypeChecker {
+  def apply(
+    graph: PredicateGraph,
+    libDefs: LibDefs,
+    additionalSubrel: Set[(PType, PType)] = Set.empty
+  ): TypeChecker = {
     val binaryRels = graph.predicates.collect {
       // inheritance is always satisfied (from definition)
       case p: BinaryRel if p.category != BinaryRelCat.inheritance => p
@@ -42,12 +52,18 @@ case class TypeChecker(
               case BinaryRelCat.subtype | BinaryRelCat.assign =>
                 Set((lhs, rhs))
               case BinaryRelCat.equal | BinaryRelCat.fixType |
-                  BinaryRelCat.fixAnnotation =>
+                   BinaryRelCat.fixAnnotation =>
                 Set((lhs, rhs), (rhs, lhs))
             }
         }
-    subtypesToCheck.filterNot {
-      case (child, parent) => context.isSubtype(child, parent, assignment)
-    }
+    val defaultContext = PTypeContext(graph, libDefs, additionalSubrel)
+    TypeChecker(
+      graph,
+      libDefs,
+      binaryRels,
+      subtypesToCheck,
+      defaultContext,
+      additionalSubrel
+    )
   }
 }
