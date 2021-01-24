2c01b7d2d6d9f050c739cf5ef17fbc942f6cf6d8
yhylord <noneofyourbusiness@example.com>
2021-01-24 21:32:00 +0800

    Add a field for method call test

--- data/tests/method-call/methodCall.ts
+++ data/tests/method-call/methodCall.ts
@@ -1,21 +1,27 @@
 class Add {
+    k: number
+    constructor(k: number) {
+        this.k = k
+    }
     add(x: number, y: number): number {
         return x + y;
     }
 }
 
 class AddWithAnonymousFunction extends Add {
-    constructor() {
-        super()
+    constructor(k: number) {
+        super(k)
         this.add = function(x: number, y: number): number {
             return x + y;
         }
     }
 }
 
-let adder = new Add()
-let anon = new AddWithAnonymousFunction()
+let adder = new Add(3)
+let anon = new AddWithAnonymousFunction(2)
 let a = 1
 let b = 3
 let c = adder.add(a, b)
 let d = anon.add(a, b)
+let e = adder.k
+let f = anon.k
