da17c993c1764bd5738cacf0e3e38cb41ce0caa3
yhylord <noneofyourbusiness@example.com>
2021-01-24 17:39:12 +0800

    Add script for analyzing CE results

--- src/main/scala/lambdanet/correctness/CEResultAnalysis.scala
+++ src/main/scala/lambdanet/correctness/CEResultAnalysis.scala
@@ -11,27 +11,39 @@ import lambdanet.translation.PredicateGraph.{BinaryRel, DefineRel, PNode, TyPred
 object CEResultAnalysis {
   def main(args: Array[String]): Unit = {
     val outputPath = amm.pwd / "CE_Results" / args(0)
-    val resultsPath =  outputPath / "ceResult.serialized"
-    val param = SM.readObjectFromFile[Params]((outputPath / "params.serialized").toIO)
+    val resultsPath = outputPath / "ceResult.serialized"
+    val param =
+      SM.readObjectFromFile[Params]((outputPath / "params.serialized").toIO)
     val inputPath = amm.pwd / "data" / param.relPathUnderData
     val (graph, nodeAnnots, results) = InputUtils.loadGraphAndPredict(inputPath)
     val monoid = implicitly[Monoid[Map[PNode, Vector[TyPredicate]]]]
-    val predicatesOf: Map[PNode, Vector[TyPredicate]] = graph.predicates.collect {
+    val predicates = graph.predicates.collect {
       case p: BinaryRel => p
       case p: DefineRel => p
-    }.map { predicate =>
-      predicate.allNodes.map(node => node -> Vector(predicate)).toMap
-    }.fold(monoid.empty)(monoid.combine)
+    }
+    val predicatesOf: Map[PNode, Vector[TyPredicate]] =
+      predicates
+        .map { predicate =>
+          predicate.allNodes.map(node => node -> Vector(predicate)).toMap
+        }
+        .fold(monoid.empty)(monoid.combine)
 
-    val ceResult = SM.readObjectFromFile[CEResult[Assignment, TypeDistrs]](resultsPath.toIO)
+    val remappedCount = predicatesOf.values.map(_.size).sum
+    val originalCount = predicates.toSeq.map(_.allNodes.size).sum
+    assert(remappedCount == originalCount)
 
+    val ceResult =
+      SM.readObjectFromFile[CEResult[Assignment, TypeDistrs]](resultsPath.toIO)
+
+    graph.nodes.toSeq.sortBy(_.getId).foreach(println)
     val groundTruth = GroundTruth(nodeAnnots, toPlainType = true)
     val groundTruthDifference: Assignment.Difference =
       Assignment.diff(results, groundTruth.truth, ceResult.elites.head)
-    groundTruthDifference.diff.foreach { case (node, (gold, pred)) =>
-      println(s"$node -> ${(gold, pred)}")
-      predicatesOf.get(node).foreach(_.foreach(println))
-      println
+    groundTruthDifference.diff.foreach {
+      case (node, (gold, pred)) =>
+        println(s"$node -> ${(gold, pred)}")
+        predicatesOf.get(node).foreach(_.foreach(println))
+        println
     }
   }
 }
