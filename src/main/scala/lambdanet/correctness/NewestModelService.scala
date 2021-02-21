e8eb990ace20062cb687beb19b249d41035f7b5b
yhylord <noneofyourbusiness@example.com>
2021-02-21 23:23:24 +0800

    Update model loading

--- src/main/scala/lambdanet/correctness/NewestModelService.scala
+++ src/main/scala/lambdanet/correctness/NewestModelService.scala
@@ -1,17 +1,17 @@
 package lambdanet.correctness
 
 import ammonite.ops.Path
-import lambdanet.TypeInferenceService.{ModelConfig, loadModel}
-import lambdanet.{Model, TypeInferenceService}
+import funcdiff.SimpleMath.readObjectFromFile
+import lambdanet.{LoadModel, Model, announced}
 
 object NewestModelService {
-  val modelDir: Path = TypeInferenceService.newestModelDir
-  val paramPath: Path = modelDir / "params.serialized"
+  val modelDir: Path = LoadModel.newestModelDir
   val modelCachePath: Path = modelDir / "model.serialized"
-  val modelConfig: ModelConfig = ModelConfig()
 
   lazy val model: Model =
-    loadModel(paramPath, modelCachePath, modelConfig, numOfThreads = 8)
+    announced(s"Loading model from $modelCachePath") {
+      readObjectFromFile[Model](modelCachePath)
+    }
   lazy val service: model.PredictionService =
     model.PredictionService(numOfThreads = 8, predictTopK = Int.MaxValue)
 }
