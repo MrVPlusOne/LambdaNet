183ca3da191c0528069207781ffc65af8439e329
koushik <koushikr17031997@gmail.com>
2021-08-03 10:47:08 +0200

    Space changes

--- src/main/scala/lambdanet/package.scala
+++ src/main/scala/lambdanet/package.scala
@@ -105,10 +105,10 @@ package object lambdanet extends SimpleMath.ExtensionsTrait {
   }
 
   case class SrcSpan(
-                      start: (Int, Int),
-                      until: (Int, Int),
-                      srcFile: ProjectPath,
-                    ) {
+      start: (Int, Int),
+      until: (Int, Int),
+      srcFile: ProjectPath,
+  ) {
     def showShort(oneBasedLineNumber: Boolean = true): String = {
       val zeroIdx = if (oneBasedLineNumber) 1 else 0
       val start1 = (start._1 + zeroIdx, start._2 + zeroIdx)
